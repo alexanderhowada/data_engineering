ba870535640aed5587b67d7c187ce9f2b1b2fbdc
Alexander Wada <alexander.wada@wmccann.com>
2023-07-20 04:55:28 -0300

    improved version of check_pep8

--- .github/workflows/check_pep8/check_pep8.sh
+++ .github/workflows/check_pep8/check_pep8.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-# Get the added or modified files.
-diff_files=$(
-  git diff --name-only origin/main origin/${GITHUB_HEAD_REF} |
-  grep -E "^[AM]\s+" |
-  grep -E --only-matching "[a-zA-Z_/ \t]+\.py$"
-)
+err_file=pycodestyle_failures
+
+# Get the added (A) or modified (M) files.
+diff_files="$(
+    git diff --name-status origin/main origin/${GITHUB_HEAD_REF} |
+    grep -E "^[AM]\s+" |
+    grep -E --only-matching "[a-zA-Z0-9_/ \t]+\.py$"
+)"
 
 # Returns if there is no modifications.
-if [ -n $diff_files ];
-then
-  echo "No modifications"
-  exit 0
+if [ ${#diff_files} -eq 0 ]; then
+    echo "No modifications"
+    exit 0
 fi
 
 # Assign the files to an array and run pycodestyle.
-read -d "\n" -a diff_files_array <<< "$diff_files"
-for f in "${diff_files_array[@]}"
+has_errors=false
+for f in "${diff_files[@]}"
 do
-  echo "------------------------------"
-  echo "Checking style of ${f}"
-  pycodestyle --max-line-length 120 --exclude=! --ignore=W,E226,E302,E402,E305 ${f}
-  echo "------------------------------"
+    echo "-----------------------------------" >> $err_file
+    echo "Checking style of ${f}" >> $err_file
+    err=$(pycodestyle --max-line-length 120 --exclude=! --ignore=W,E226,E302,E402,E305 ${f})
+    if [ ${#diff_files} -gt 0 ]; then
+        has_errors=true
+        echo "$err" >> $err_file
+    fi
+    echo "-----------------------------------" >> $err_file
 done
 
 # Exits with status 1 in case of errors.
-failures(){
-  echo "PEP 8 problems."
-  exit 1
-}
-trap "failures" ERR
+if [ $has_errors=true ]; then
+    cat $err_file
+    rm -rf $err_file
+    exit 1
+fi
 
+rm -rf $err_file
 echo "OK!"

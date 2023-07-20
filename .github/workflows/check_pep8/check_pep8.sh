51a2b009ad5708d420c10ae6c825e95ae76d4d72
Alexander Wada <alexander.wada@wmccann.com>
2023-07-20 02:51:28 -0300

    added origin

--- .github/workflows/check_pep8/check_pep8.sh
+++ .github/workflows/check_pep8/check_pep8.sh
@@ -4,7 +4,7 @@ set -e
 
 # Get the added or modified files.
 diff_files=$(
-  git diff --name-only main ${GITHUB_HEAD_REF} |
+  git diff --name-only origin/main origin/${GITHUB_HEAD_REF} |
   grep -E "^[AM]\s+" |
   grep -E --only-matching "[a-zA-Z_/ \t]+\.py$"
 )

c8df2786f2abe6300aa5f80798d852dcc190819c
Alexander Wada <alexander.wada@wmccann.com>
2023-07-24 00:03:16 -0300

    changed unittest

--- .github/workflows/unittest/unittest.sh
+++ .github/workflows/unittest/unittest.sh
@@ -2,7 +2,7 @@
 
 # Example command.
 # GITHUB_HEAD_REF=main LOG_FILE=.github/workflows/docker_logs/unittest.log bash .github/workflows/unittest/unittest.sh
-git checkout origin/$GITHUB_HEAD_REF
+git checkout $GITHUB_HEAD_REF
 
 python3 -m unittest discover -s . -v &> $LOG_FILE
 

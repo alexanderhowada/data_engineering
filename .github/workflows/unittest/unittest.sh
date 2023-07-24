043782ef89a1e66fefb4ad3bc0808a8d945a4433
Alexander Wada <alexander.wada@wmccann.com>
2023-07-23 23:55:00 -0300

    fixed unittest

--- .github/workflows/unittest/unittest.sh
+++ .github/workflows/unittest/unittest.sh
@@ -2,11 +2,13 @@
 
 # Example command.
 # GITHUB_HEAD_REF=main LOG_FILE=.github/workflows/docker_logs/unittest.log bash .github/workflows/unittest/unittest.sh
-git checkout origin/$GITHUB_HEAD_REF
+git checkout $GITHUB_HEAD_REF
 
 python3 -m unittest discover -s . -v &> $LOG_FILE
 
 err=$(cat $LOG_FILE | grep -E "^FAILED \(errors=[0-9]+\)")
 if [ ${#err} -gt 0 ]; then
-    :> $LOG_FILE
+    exit 1
 fi
+
+:> $LOG_FILE

5edf1face92b07b48e073aef0d24e9b899ed29e9
Alexander Wada <alexander.wada@wmccann.com>
2023-08-22 11:01:55 -0300

    create or update lambda function

--- aws/aws_lambda/deploy.sh
+++ aws/aws_lambda/deploy.sh
@@ -9,19 +9,27 @@ fi
 
 TARGET_FUNCTION="$1"
 
-rm -rf package
-mkdir package
-
-cp -r ${TARGET_FUNCTION}/* package/.
-pip3 install -r "${TARGET_FUNCTION}/requirements.txt" -t ./package
+#rm -rf package
+#mkdir package
+#
+#cp -r ${TARGET_FUNCTION}/* package/.
+#pip3 install -r "${TARGET_FUNCTION}/requirements.txt" -t ./package
 
 cd package
-zip -r ${TARGET_FUNCTION}.zip .
+# zip -r ${TARGET_FUNCTION}.zip .
 
 # aws s3 cp ${TARGET_FUNCTION}.zip s3://${AWS_LAMBDA_BUCKET}/${TARGET_FUNCTION}.zip
-aws lambda create-function --function-name ${TARGET_FUNCTION} \
-    --runtime python3.11 \
-    --handler main.main \
-    --role ${AWS_LAMBDA_ROLE} \
-    --zip-file fileb://${TARGET_FUNCTION}.zip
+
+# Check if function exists.
+aws lambda get-function --function-name first_lambda > /dev/null
+if [ 0 -eq $? ]; then
+    aws lambda update-function-code --function-name ${TARGET_FUNCTION} \
+        --zip-file fileb://${TARGET_FUNCTION}.zip
+else
+    aws lambda create-function --function-name ${TARGET_FUNCTION} \
+        --runtime python3.11 \
+        --handler main.main \
+        --role ${AWS_LAMBDA_ROLE} \
+        --zip-file fileb://${TARGET_FUNCTION}.zip
+fi
 

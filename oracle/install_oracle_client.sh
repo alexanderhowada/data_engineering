774058df0010f7ff1269fb8fa052ce925d9336c2
Alexander Wada <alexander.wada@wmccann.com>
2023-08-22 01:08:24 -0300

    changed comment

--- oracle/install_oracle_client.sh
+++ oracle/install_oracle_client.sh
@@ -4,7 +4,7 @@ set -e
 
 apt update -y && apt install libaio1 alien -y
 
-# Downloads the latest oracle file.
+# Downloads the latest oracle file and install it.
 wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-basic-linuxx64.rpm
 f_name=$(alien --to-deb oracle-instantclient-basic-linuxx64.rpm)
 f_name=$(echo $f_name | sed -E 's/\sgenerated//')

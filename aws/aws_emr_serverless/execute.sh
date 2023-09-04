0a43d4ec9f27c41e23662ad8e0ebffff406a7713
Alexander Wada <alexander.wada@wmccann.com>
2023-09-04 01:51:35 -0300

    execution example

--- aws/aws_emr_serverless/execute.sh
+++ aws/aws_emr_serverless/execute.sh
@@ -16,7 +16,7 @@ aws emr-serverless start-job-run \
         "sparkSubmit": {
             "entryPoint": "s3://ahow-delta-lake/scripts/main.py",
             "entryPointArguments": [],
-            "sparkSubmitParameters": "--conf spark.jars=s3://ahow-delta-lake/delta-core_2.12-2.4.0.jar,s3://ahow-delta-lake/delta-storage-2.4.0.jar --conf spark.executor.cores=1 --conf spark.executor.memory=2g --conf spark.driver.cores=1 --conf spark.driver.memory=2g --conf spark.executor.instances=1 --conf spark.archives=s3://ahow-delta-lake/emr_serverless_packages.tar.gz#environment --conf spark.emr-serverless.driverEnv.PYSPARK_DRIVER_PYTHON=./environment/bin/python --conf spark.emr-serverless.driverEnv.PYSPARK_PYTHON=./environment/bin/python --conf spark.executorEnv.PYSPARK_PYTHON=./environment/bin/python"
+            "sparkSubmitParameters": "--conf spark.hadoop.hive.metastore.client.factory.class=com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory --conf spark.jars=s3://ahow-delta-lake/delta-core_2.12-2.4.0.jar,s3://ahow-delta-lake/delta-storage-2.4.0.jar --conf spark.executor.cores=1 --conf spark.executor.memory=2g --conf spark.driver.cores=1 --conf spark.driver.memory=2g --conf spark.executor.instances=1 --conf spark.archives=s3://ahow-delta-lake/emr_serverless_packages.tar.gz#environment --conf spark.emr-serverless.driverEnv.PYSPARK_DRIVER_PYTHON=./environment/bin/python --conf spark.emr-serverless.driverEnv.PYSPARK_PYTHON=./environment/bin/python --conf spark.executorEnv.PYSPARK_PYTHON=./environment/bin/python"
         }
     }'
 

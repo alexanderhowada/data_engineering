8068e9ec9626c48ce0fb9f346a08630b9bc0ff57
Alexander Wada <alexander.wada@wmccann.com>
2023-09-06 02:34:41 -0300

    trying to fix workflow

--- .github/workflows/unittest/unittest.sh
+++ .github/workflows/unittest/unittest.sh
@@ -4,7 +4,7 @@
 # GITHUB_HEAD_REF=main LOG_FILE=.github/workflows/docker_logs/unittest.log bash .github/workflows/unittest/unittest.sh
 git checkout $GITHUB_HEAD_REF
 
-python3 -m unittest discover -s . -v &> $LOG_FILE
+PYTHONPATH='.' python3 -m unittest discover -s . -v &> $LOG_FILE
 
 err=$(cat $LOG_FILE | grep -E "^FAILED \(errors=[0-9]+\)")
 if [ ${#err} -gt 0 ]; then
